f6d6220fe2d447223d450a0426977500169b4ad6
leeseungnam1234 <107826488+leeseungnam1234@users.noreply.github.com>
2023-03-26 21:50:25 +0900

    Add files via upload

--- views/HomeView.vue
+++ views/HomeView.vue
@@ -5,16 +5,7 @@
     <div>
     <img class="logo" alt="Vue logo" src="../assets/baeminlogo.png">
     <img class="logo2" alt="Vue baemins" src="@/assets/배민외식업광장.jpg">
-      <!-- 검색창 -->
-      <div class="search-container">
-        <input type="text" v-model="query" @input="search" placeholder="원하는 메뉴를 검색해보세요.">
-        <i class="search-icon fas fa-search" @click="search"></i>
-        <ul v-if="results.length">
-          <li v-for="(result, index) in results" :key="index">{{ result }}</li>
-        </ul>
-      </div>
-      <!-- 검색창 -->
-      <!-- 장사 캘린더 ~ 식재료 비품 -->
+    <!-- 장사 캘린더 ~ 식재료 비품 -->
       <div>
         <ul class="navigation">
           <li v-for="(item, index) in navigationItems" :key="index">
@@ -38,32 +29,50 @@
     </div>
     <!-- header -->
     <!-- body -->
-    <div>
-      <div class="Member__LoginBox">
-        <p class="h6 mb-1">로그인을 해주세요.</p>
-        <a target="_self" block="1" class="Button__StyledButton" href="">배민외식업광장 로그인</a>
-        <div class="Member__Links">
-          <a href="/find-account">아이디·비밀번호 찾기</a>
-          <a href="/join">회원가입</a>
+    <div class="Body">
+        <!-- 검색창 -->
+        <div class="search-container">
+          <input type="text" v-model="query" @input="search" placeholder="원하는 메뉴를 검색해보세요.">
+          <i class="search-icon fas fa-search" @click="search"></i>
+          <ul v-if="results.length">
+            <li v-for="(result, index) in results" :key="index">{{ result }}</li>
+          </ul>
         </div>
-      </div>
-    </div>
-    <!-- 슬라이드 -->
-    <div>
-      <div class="slider-container">
-        <div class="slider">
-          <div class="slides" :style="`transform:translateX(-${currentSlideIndex * slideWidth}px)`">
-            <div v-for="(slide, index) in slides" :key="index" class="slide">
-              {{ slide }}
+        <!-- 검색창 -->
+        <div class="body_right">
+          <!-- 로그인창 -->
+          <div class="Member__LoginBox">
+            <p class="h6 mb-1">로그인을 해주세요.</p>
+            <a target="_self" block="1" class="Button__StyledButton" href="">배민외식업광장 로그인</a>
+            <div class="Member__Links">
+              <a href="/find-account">아이디·비밀번호 찾기</a>
+              <a href="/join">회원가입</a>
             </div>
           </div>
+          <!-- 로그인창 -->
+          <!-- 사전신청 -->
+          <div class="bodyEvent">
+            <a target="_blank" class="styled event1" href="/event">
+              <p class="styled_ event2">사전 신청하면 선물도 드려요</p>
+              <h2 color="#fff" class="styled_ event3">배민외식업박람회 2023<br></h2>
+            </a>
+          </div>
+          <!-- 사전신청 -->
         </div>
-        <button @click="prevSlide">Previous</button>
-        <h3>현재 슬라이드 제작중 입니다.</h3>
-        <button @click="nextSlide">Next</button>
-      </div>
     </div>
-    <!-- 슬라이드 -->
+    <div class="body_lfet">
+      <!-- 슬라이드 -->
+      <div>
+            <div class="slider">
+              <img :src="currentImage" :alt="currentImageAlt" class="slider__image">
+              <div class="slider__controls">
+                <button class="slider__button" @click="prevImage" :disabled="currentIndex === 0">이전</button>
+                <button class="slider__button" @click="nextImage" :disabled="currentIndex === images.length - 1">다음</button>
+              </div>
+            </div>
+          </div>
+          <!-- 슬라이드 -->
+    </div>
     <!-- body -->
   </div>
 </template>
@@ -103,9 +112,25 @@ export default {
       ],
       // 장사캘린더~ 배민비즈니스
       // 슬라이드
-      currentSlideIndex: 0,
-      slides: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'],
-      slideWidth: 500
+      images: [
+        {
+          src: require('@/assets/삼겹살.jpg'),
+          alt: 'Image 1'
+        },
+        {
+          src: require('@/assets/양고기.jpg'),
+          alt: 'Image 2'
+        },
+        {
+          src: require('@/assets/햄버거.jpg'),
+          alt: 'Image 3'
+        },
+        {
+          src: require('@/assets/도넛.jpg'),
+          alt: 'Image 4'
+        }
+      ],
+      currentIndex: 0
       // 슬라이드
     }
   },
@@ -125,15 +150,25 @@ export default {
       // 검색 서버연결
     },
     // 슬라이드
-    nextSlide () {
-      this.currentSlideIndex =
-        (this.currentSlideIndex + 1) % this.slides.length
+    prevImage () {
+      if (this.currentIndex > 0) {
+        this.currentIndex -= 1
+      }
     },
-    prevSlide () {
-      this.currentSlideIndex =
-        (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length
+    nextImage () {
+      if (this.currentIndex < this.images.length - 1) {
+        this.currentIndex += 1
+      }
     }
     // 슬라이드
+  },
+  computed: {
+    currentImage () {
+      return this.images[this.currentIndex].src
+    },
+    currentImageAlt () {
+      return this.images[this.currentIndex].alt
+    }
   }
 }
 </script>
@@ -391,68 +426,114 @@ li a[href='/business'] {
 }
 /* 로그인 */
 /* 슬라이드 */
-.slider-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
 .slider {
-  width: 100%;
-  max-width: 800px;
-  height: 200px;
-  margin-top: 30px;
+  display: block;
   position: relative;
+  width: 1000px;
+  height: 400px;
+  margin-left: 15px;
+  margin-top: -190px
 }
 
-.slides {
-  display: flex;
-  height: 100%;
-  transition: transform 0.3s ease-in-out;
-}
-
-.slide {
+.slider__image {
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 32px;
-  color: #6c0bdb;
+  object-fit: cover;
 }
 
-.slide:nth-child(odd) {
-  background-color: #219c0e;
-}
-
-.slide:nth-child(even) {
-  background-color: #0dd127;
-}
-
-.slide::before {
-  content: "";
+.slider__controls {
   position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  background-color: #1b4f55;
-  transform: skewX(-30deg);
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
 }
 
-.slide:first-child::before {
-  background-color: #bd1818;
+.slider__button {
+  margin: 0 10px;
+  background-color: #00e1ff;
+  border: 1px solid #000;
+  padding: 1px;
+  cursor: pointer;
+  outline: none;
 }
 
-.slide:last-child::before {
-  background-color: #ade5a0;
-}
-
-.slide-text {
-  position: relative;
-  z-index: 1;
-  padding: 20px;
+.slider__button:disabled {
+  opacity: 0.3;
+  cursor: not-allowed;
 }
 /* 슬라이드 */
+/* event */
+.event1 {
+  display: inline-block;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  justify-content: center;
+  min-height: 80px;
+  background: url(https://ceoimg.cdn.baemin.com/ceoimg-ym4g/i/2023/3/10/01gv585sqd2vce2tewcxwnrea7.png) right 24px bottom 50% / auto 100% no-repeat rgb(81, 114, 255);
+  padding: 14px 112px 14px 24px;
+  border-radius: 4px;
+  width: 350px;
+  margin-left: 80%;
+  margin-top: 5px;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+  background-color: transparent;
+}
+
+a:-webkit-any-link {
+  color: -webkit-link;
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.event2 {
+  opacity: 0.88;
+  font-size: 12px;
+  line-height: 20px;
+  letter-spacing: -0.2px;
+  color: rgb(255, 255, 255);
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  -webkit-line-clamp: 1;
+}
+
+.event3 {
+  color: rgb(255, 255, 255);
+  font-weight: 500;
+  font-size: 16px;
+  letter-spacing: -0.4px;
+  line-height: 24px;
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-height: 3em;
+  -webkit-line-clamp: 2;
+}
+
+h2 {
+  display: block;
+  font-size: 1.5em;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+}
+
+h1, .h1, h2, .h2, h3, .h3, h4, .h4, h5, .h5, h6, .h6 {
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-weight: normal;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-weight: normal;
+}
+/* event */
 </style>
